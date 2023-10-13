e2447ac67e900780d89a2ee0a13e244589db2861
Chukwudi Nwobodo <Chukwudi.Nwobodo@qualtex-global.com>
2023-10-13 17:51:50 +0100

    Corrects failed test

--- TestApi.UnitTest/TestCar.cs
+++ TestApi.UnitTest/TestCar.cs
@@ -12,7 +12,6 @@ namespace TestApi.UnitTest
             Assert.Equal(a, car.Name);
             Assert.Equal(b, car.Color);
             Assert.Equal(c, car.YearManufactured);
-            Assert.True(false);
         }
     }
 }
